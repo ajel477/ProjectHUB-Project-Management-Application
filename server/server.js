1cad941bb2bbe255659c490bc91544f8f3f37b94
ajel477 <ajelmathew04@gmail.com>
2026-07-09 16:02:40 +0530

    server config

--- server/server.js
+++ server/server.js
@@ -11,34 +11,32 @@ import dashboardRoutes from "./routes/dashboardRoutes.js";
 import notFound from "./middleware/notFound.js";
 import errorHandler from "./middleware/errorHandler.js";
 
-// Load environment variables
 dotenv.config();
 
-// Connect to MongoDB
 connectDB();
 
-// Initialize Express app
 const app = express();
 
-// Middleware
-app.use(cors());
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 app.use(express.json());
 
-//Routes
 app.use("/api/projects", projectRoutes);
 app.use("/api", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
-//Unknown Routes
 app.use(notFound);
 
-// Global Error Handler
 app.use(errorHandler);
 
-// Port
 const PORT = process.env.PORT || 5000;
 
-// Start Server
+
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
